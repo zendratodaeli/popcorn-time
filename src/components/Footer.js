68f7b9d5f33100aa18df6c19a94a2903d5a9a45c
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-06-20 12:47:25 +0200

    Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return(
+    <div>
+      <footer>Hello, this is a Footer</footer>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return(
+    <div>
+      <section>Hello, this is a Main</section>
+    </div>
+  )
+}
+
+export default Main;
